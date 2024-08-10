5c82ea0abe2dea3ff1501a53db9f6b783fff5285
Tommy Chen <tommyjchen@hotmail.com>
2024-08-10 09:40:02 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Betrayal at the House on the Hill</h1>
+      <button>Login</button>
+      <button>Sign Up</button>
+    </>
+  )
+}
+
+export default App
