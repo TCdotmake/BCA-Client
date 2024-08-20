85bb8fae842f59bd84cc93fd4e585c80ba5f287e
Tommy Chen <tommyjchen@hotmail.com>
2024-08-19 22:24:58 -0400

    post user working for signup

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -3,32 +3,35 @@ import { useEffect, useState } from "react";
 
 const PASSMINLEN = 7;
 const NAMEMINLEN = 3;
-
-function Signup(){
+const URL = 'http://localhost:3000'
+export default function Signup(){
 const [email, setEmail] = useState<string>("");
 const [name, setName] = useState<string>("");
-const [pass, setPass] = useState<string>("");
+const [password, setPassword] = useState<string>("");
 const [confirmPass, setConfirmPass] = useState<string>("");
 
 const handleEmail = (e:React.ChangeEvent<HTMLInputElement>):void=>{setEmail(e.target.value)}
 const handleName = (e:React.ChangeEvent<HTMLInputElement>):void=>{setName(e.target.value)}
-const handlePass = (e:React.ChangeEvent<HTMLInputElement>):void=>{setPass(e.target.value)}
+const handlePass = (e:React.ChangeEvent<HTMLInputElement>):void=>{setPassword(e.target.value)}
 const handleConfirmPass = (e:React.ChangeEvent<HTMLInputElement>):void=>{setConfirmPass(e.target.value)}
 
 useEffect(()=>{
     if(validator.isEmail(email) &&
        name.length >= NAMEMINLEN &&
-       pass.length >= PASSMINLEN &&
-       confirmPass === pass
+       password.length >= PASSMINLEN &&
+       confirmPass === password
 ){document.getElementById("signupSubmit")?.removeAttribute('disabled')}
 else{document.getElementById("signupSubmit")?.setAttribute('disabled', "true")}
-},[email, name, pass, confirmPass])
+},[email, name, password, confirmPass])
 
-const handleSubmit = ()=>{console.log('Signup submit')}
+const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+    sendSignup({email,name, password}, URL)
+}
 
 return(
     <>
-        <form>
+        <form onSubmit={handleSubmit}>
 			<label>Email</label>
 			<input type="email" onChange={handleEmail} />
             <label>User Name</label>
@@ -37,11 +40,34 @@ return(
 			<input type="password" onChange={handlePass} />
             <label>Confirm Password</label>
 			<input type="password" onChange={handleConfirmPass} />
-			<input type='submit' value="Submit" disabled onClick={handleSubmit} id="signupSubmit"/>
+			<input type='submit' value="Submit" id="signupSubmit"/>
 			</form>
 
     </>
 )
 }
 
-export default Signup;
+interface Usersignup{
+    email: string,
+    name: string,
+    password: string
+}
+
+function sendSignup(user:Usersignup, url:string){
+    const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify(user);
+
+const requestOptions: RequestInit = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch(`${url}/user`, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+}
